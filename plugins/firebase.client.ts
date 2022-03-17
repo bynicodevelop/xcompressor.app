11f6584bb9a5f20b38fade93bdd7d7062495df71
nico-develop <contact@nico-develop.fr>
2022-03-17 10:02:08 +0100

    Permet l'enregistrement du nombre de fichier et co2 en BDD

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,19 +1,20 @@
 import { defineNuxtPlugin } from '#app';
 import { useFirebase } from '~~/composables/useFirebase';
 import UserRepository from '~~/repositories/UserRepository';
+import UploadStatsRepository from '~~/repositories/UploadStatsRepository';
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   console.log("Plugin: Firebase");
 
   const sessionCookie = useCookie<Object>('__session');
 
-  const { AUTHENTICATED_USER_MAX_UPLOAD_PER_DAY, AUTHENTICATED_USER_MAX_FILE_SIZE } =
-    useRuntimeConfig();
-
-  const { auth } = useFirebase();
+  const { auth, firestore } = useFirebase();
 
   const userRepository = new UserRepository(auth);
   useState('userRepository', () => userRepository);
+
+  const uploadStatsRepository = new UploadStatsRepository(firestore);
+  useState('uploadStatsRepository', () => uploadStatsRepository);
 
   const user = await userRepository.getCurrentUser();
   const userState = useState('user', () => user);
